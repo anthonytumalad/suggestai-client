50d19e836069c835cf1347e4e9f9c1ab89716895
happynewyeareveryone <165658644+happynewyeareveryone@users.noreply.github.com>
2026-01-24 13:50:50 +0800

    Clean up base app and prepare branch for login feature

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
-import './assets/main.css'
+import './assets/css/main.css'
 
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import { queryClient } from './lib/queryClient'
+import ToastService from 'primevue/toastservice'
+import Toast from "primevue/toast"
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 import App from './App.vue'
 import router from './router'
+import Input from './components/ui/input/Input.vue'
+import AppLogo from './components/AppLogo.vue'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueQueryPlugin, { queryClient })
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'none',
+    }
+  }
+})
+app.use(ToastService)
+
+app.component('AppInput', Input)
+app.component('AppLogo', AppLogo)
+app.component("AppToast", Toast)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { routerGuard } from './guard'
+import ViewLogin from '@/pages/ViewLogin.vue'
+import ViewDashboard from '@/pages/ViewDashboard.vue'
+import ViewFormList from '@/pages/forms/ViewFormList.vue'
+import ViewFormSummary from '@/pages/forms/ViewFormSummary.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: () => import('@/layouts/AppLayout.vue'),
+      children: [
+        {
+          path: '',
+          redirect: { name: 'dashboard' }
+        },
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: ViewDashboard,
+
+        },
+        {
+          path: 'forms',
+          name: 'forms',
+          redirect: '/forms/list',
+          component: () => import('@/layouts/FormLayout.vue'),
+          children: [
+            {
+              path: 'list',
+              name: 'forms-list',
+              component: ViewFormList
+            },
+            {
+              path: 'summary',
+              name: 'forms-summary',
+              component: ViewFormSummary,
+            }
+          ]
+
+        }
+      ],
+      meta: {
+        requiresAuth: true,
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/auth/login',
+      name: 'login',
+      component: ViewLogin,
+      meta: {
+        title: 'Login',
+        guest: true,
+      }
     },
   ],
 })
+
+routerGuard(router)
 
 export default router
